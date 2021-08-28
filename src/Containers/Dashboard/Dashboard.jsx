6131f1abf1be8687ef12189595b1c58693c6dcca
NotAngad <srivastav.angad@gmail.com>
2021-08-28 16:14:45 +0530

    Table issue fixed

--- src/Containers/Dashboard/Dashboard.jsx
+++ src/Containers/Dashboard/Dashboard.jsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 import { Grid, Toolbar } from "@material-ui/core";
 import { Pie } from "react-chartjs-2";
+import TableComponent from "../../Components/Table/Table";
+import CardComponent from "../../Components/Cards/Cards";
 
 const data = {
   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -42,132 +31,15 @@ const data = {
   ],
 };
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-  table: {
-    minWidth: 340,
-  },
-  tableCell: {
-    paddingRight: 4,
-    paddingLeft: 5,
-  },
-});
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function Dashbaord() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
-
   return (
     <>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={12} lg={4}>
-          <Card elevation={5} className={classes.root}>
-            <CardContent>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="h2">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography className={classes.pos} color="textSecondary">
-                adjective
-              </Typography>
-              <Typography variant="body2" component="p">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={12} lg={4}>
-          <Card elevation={5} className={classes.root}>
-            <CardContent>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="h2">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography className={classes.pos} color="textSecondary">
-                adjective
-              </Typography>
-              <Typography variant="body2" component="p">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={12} lg={4}>
-          <Card elevation={5} className={classes.root}>
-            <CardContent>
-              <Typography
-                className={classes.title}
-                color="textSecondary"
-                gutterBottom
-              >
-                Word of the Day
-              </Typography>
-              <Typography variant="h5" component="h2">
-                be{bull}nev{bull}o{bull}lent
-              </Typography>
-              <Typography className={classes.pos} color="textSecondary">
-                adjective
-              </Typography>
-              <Typography variant="body2" component="p">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        {[0, 1, 2].map((item) => (
+          <Grid key={item} item xs={12} sm={12} lg={4}>
+            <CardComponent />
+          </Grid>
+        ))}
       </Grid>
 
       <Toolbar />
@@ -195,58 +67,7 @@ export default function Dashbaord() {
       </div>
 
       <Toolbar />
-      <Grid container>
-        <Grid item xs={12}>
-          <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell className={classes.tableCell}>
-                    Dessert (100g serving)
-                  </TableCell>
-                  <TableCell className={classes.tableCell} align="right">
-                    Calories
-                  </TableCell>
-                  <TableCell className={classes.tableCell} align="right">
-                    Fat&nbsp;(g)
-                  </TableCell>
-                  <TableCell className={classes.tableCell} align="right">
-                    Carbs&nbsp;(g)
-                  </TableCell>
-                  <TableCell className={classes.tableCell} align="right">
-                    Protein&nbsp;(g)
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.name}>
-                    <TableCell
-                      className={classes.tableCell}
-                      component="th"
-                      scope="row"
-                    >
-                      {row.name}
-                    </TableCell>
-                    <TableCell className={classes.tableCell} align="right">
-                      {row.calories}
-                    </TableCell>
-                    <TableCell className={classes.tableCell} align="right">
-                      {row.fat}
-                    </TableCell>
-                    <TableCell className={classes.tableCell} align="right">
-                      {row.carbs}
-                    </TableCell>
-                    <TableCell className={classes.tableCell} align="right">
-                      {row.protein}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-      </Grid>
+      <TableComponent />
     </>
   );
 }
